f581598eb9f664121393a321f13fb65c70ea5d6a
Lucas Vieira <lucas.vieira.dasilva@outlook.com>
2020-07-21 07:36:09 -0300

    commit inicial

---
+++ src/test/java/br/com/unnamed/spring_cloud_discovery_server/SpringCloudDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unnamed.spring_cloud_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
